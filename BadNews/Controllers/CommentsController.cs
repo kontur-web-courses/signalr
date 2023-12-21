4b6c2c478c6794b6b00308899cfd454778a81ecc
dryelm <rinat.gilemshinn@gmail.com>
2023-12-21 13:26:25 +0500

    done

---
+++ BadNews/Controllers/CommentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using BadNews.Models.Comments;
+using BadNews.Repositories.Comments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Controllers
+{
+	[ApiController]
+	public class CommentsController : ControllerBase
+	{
+		private readonly CommentsRepository commentsRepository;
+
+		public CommentsController(CommentsRepository commentsRepository)
+		{
+			this.commentsRepository = commentsRepository;
+		}
+
+		// GET
+		[HttpGet("api/news/{id}/comments")]
+		public ActionResult<CommentsDto> GetCommentsForNews(Guid newsId)
+		{
+			return Ok(commentsRepository.GetComments(newsId));
+		}
+	}
+}
